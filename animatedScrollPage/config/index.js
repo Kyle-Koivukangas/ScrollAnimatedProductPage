76962998418da5778fe8f093ef9a210ffa8cef69
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2018-04-09 01:22:47 -0700

    Github pages public assets path changed. Okay, I think I finally figured out how webpack does the pathing for assets..

--- animatedScrollPage/config/index.js
+++ animatedScrollPage/config/index.js
@@ -40,8 +40,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, "../dist"),
-    assetsSubDirectory: "ScrollAnimatedProductPage", // <-- Custom asset url path for gh-pages
-    assetsPublicPath: "/",
+    assetsSubDirectory: "static",
+    assetsPublicPath: "/ScrollAnimatedProductPage/", // <-- Custom asset url path for gh-pages
 
     /**
      * Source Maps
